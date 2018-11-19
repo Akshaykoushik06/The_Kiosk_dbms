ee37d4d9fd98f74a6e55bd5861c858afa532b174
Ritanya <ritanyabhaskar31@gmail.com>
2018-11-19 15:56:21 +0530

    updated database

--- pyfiles/test.py
+++ pyfiles/test.py
@@ -1,1 +1,9 @@
-import flask
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/process',method=['POST'])
+def process():
+    name=request.form['name']
+    usn=request.form['usn']
+    return render_template('new_student.html',name=name,usn=usn)
+if __name__ == '__main__':
+    app.run(debug=True)
